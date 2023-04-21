16a8e47ebdb66234db7d9fe87715190800bbad25
Brad_Developer <69091615+BradDeveloper@users.noreply.github.com>
2023-04-21 02:28:11 -0500

    Update Network.lua

--- Network.lua
+++ Network.lua
@@ -1,68 +1,111 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+local IS_SERVER = RunService:IsServer()
 
 --assertion-properties
 local types = {"RemoteEvent", "RemoteFunction", "BindableEvent", "BindableFunction"}
-local fires = {"FireServer", "FireClient", "FireAllClients", "InvokeServer", "InvokeClient", "Fire", "Invoke"}
-local wraps = {"OnClientEvent", "OnServerEvent", "OnClientInvoke", "OnServerInvoke", "Event", "Wait", "OnInvoke"}
 --
 
-local Events = ReplicatedStorage.BEngine.Storage.Events
-local network = {}
+local Events = Instance.new("Folder")
+Events.Name = "NetworkEvents"
+Events.Parent = ReplicatedStorage
 
-function network:Create(name: string, typ: string): any
-    assert(name and type(name) == "string" and typ and type(typ) == "string" and table.find(types, typ), "net:Create failed")
+local Network = {}
+
+--Handles network creation
+function Network:Create(name: string, typ: string): (RemoteEvent | RemoteFunction | BindableEvent | BindableFunction)
+    assert(type(name) == "string" and typ and type(typ) == "string" and table.find(types, typ), "net:Create failed")
     local event = Instance.new(typ)
     event.Name = name
     event.Parent = Events
     return event
 end
 
-function network:Fire(name: string, call: string, ...): boolean
-    assert(name and type(name) == "string" and call and type(call) == "string" and table.find(fires, call), "net:Fire failed")
-    local event = Events:FindFirstChild(name)
+--Handles general remote firing
+function Network:Fire(name: string, ...)
+    local event = Events:WaitForChild(name, 5)
     if event then
-        if call == "FireServer" then
-            event:FireServer(...)
-        elseif call == "FireClient" then
-            event:FireClient(...)
-        elseif call == "InvokeServer" then
-            return event:InvokeServer(...)
-        elseif call == "InvokeClient" then
-            return event:InvokeClient(...)
-        elseif call == "FireAllClients" then
-            event:FireAllClients(...)
-        elseif call == "Fire" then
-            event:Fire(...)
-        elseif call == "Invoke" then
-            event:Invoke(...)
+        if event:IsA("BindableEvent") then
+            event:Fire(...)            
+        elseif event:IsA("RemoteEvent") then
+            if IS_SERVER then
+                event:FireClient(...)
+            else
+                event:FireServer(...)
+            end
+        else
+            error("cannot handle remote type")
         end
     else
-        return error("failed to find")
+        return error("failed to find" .. name)
     end
 end
 
-function network:Wrap(name: string, call: string, funct: RBXScriptConnection)
-    assert(name and type(name) == "string" and call and type(call) == "string" and table.find(wraps, call) and funct and typeof(funct) == "function","net:Wrap failed")
-    local event = Events:FindFirstChild(name)
+--Handles remote firing for all
+function Network:FireAll(name: string, ...)
+    local event = Events:WaitForChild(name, 5)
     if event then
-        if call == "OnClientEvent" then
-            return event.OnClientEvent:Connect(funct)
-        elseif call == "OnServerEvent" then
-            return event.OnServerEvent:Connect(funct)
-        elseif call == "Event" then
+        event:FireAllClients(...)
+    else
+        return error("failed to find" .. name)
+    end
+end
+
+--Handles network wait event
+function Network:Wait(name: string): any
+    local event = Events:WaitForChild(name, 5)
+    if event then
+        return event:Wait()
+    else
+        return error("failed to find" .. name)
+    end
+end
+
+--Handles remote invokes
+function Network:Invoke(name: string, ...): any
+    local event = Events:WaitForChild(name, 5)
+    if event then
+        if event:IsA("RemoteFunction") then
+            if IS_SERVER then
+                return event:InvokeClient(...)
+            else
+                return event:InvokeServer(...)
+            end
+        elseif event:IsA("BindableFunction") then
+            return event:Invoke(...)
+        else
+            return error("failed to find" .. name)
+        end
+    end
+end
+
+--Handles remote wrappers (events and invokes)
+function Network:Wrap(name: string, funct: RBXScriptConnection): RBXScriptConnection
+    local event = Events:WaitForChild(name, 5)
+    if event then
+        if event:IsA("BindableEvent") then
             return event.Event:Connect(funct)
-        elseif call == "OnClientInvoke" then
-            event.OnClientInvoke = funct
-        elseif call == "OnServerInvoke" then
-            event.OnServerInvoke = funct
-        elseif call == "Wait" then
-            return event:Wait()
-        elseif call == "OnInvoke" then
+        elseif event:IsA("RemoteEvent") then
+            if IS_SERVER then
+                return event.OnServerEvent:Connect(funct)
+            else
+                return event.OnClientEvent:Connect(funct)
+            end
+        elseif event:IsA("BindableFunction") then
             event.OnInvoke = funct
+        elseif event:IsA("RemoteFunction") then
+            if IS_SERVER then
+                event.OnServerInvoke = funct
+            else
+                event.OnClientInvoke = funct
+            end
+        else
+            error("cannot handle remote type")
         end
     else
-        return error("failed to find")
+        return error("failed to find" .. name)
     end
 end
 
-return network
+return Network
